987b6a0d656de811927fb27d570eb97c16b5ebad
UDAY-2997 <udaythakur.shely@gmail.com>
2023-10-10 18:44:11 -0500

    Unit test for Get Recipes By Ingredient Valid Name Returns Recipes With Ingredient pass

--- RecipeUnitTests/UnitTest1.cs
+++ RecipeUnitTests/UnitTest1.cs
@@ -27,7 +27,49 @@ namespace RecipeUnitTests
         [TestMethod]
         public void GetRecipesByIngredient_ValidName_ReturnsRecipesWithIngredient()
         {
+            // Arrange
+            AppStorage appStorage = new AppStorage(); // Initialize your app storage
+            BusinessLogicLayer bll = new BusinessLogicLayer(appStorage); // Initialize the BusinessLogicLayer
 
+            // Create a test ingredient
+            Ingredient ingredient = new Ingredient { Id = 1, Name = "Spaghetti" };
+            appStorage.Ingredients.Add(ingredient);
+
+            // Create test recipes with the ingredient
+            Recipe recipe1 = new Recipe { Id = 1, Name = "Spaghetti Carbonara" };
+            Recipe recipe2 = new Recipe { Id = 2, Name = "Spaghetti Bolognese" };
+            Recipe recipe3 = new Recipe { Id = 3, Name = "Lasagna" };
+
+            // Add the recipes to the app storage
+            appStorage.Recipes.Add(recipe1);
+            appStorage.Recipes.Add(recipe2);
+            appStorage.Recipes.Add(recipe3);
+
+            // Create RecipeIngredient objects to associate recipes with the ingredient
+            RecipeIngredient recipeIngredient1 = new RecipeIngredient
+            {
+                IngredientId = ingredient.Id,
+                RecipeId = recipe1.Id,
+                Amount = 200,
+                MeasurementUnit = MeasurementUnit.Grams
+            };
+            RecipeIngredient recipeIngredient2 = new RecipeIngredient
+            {
+                IngredientId = ingredient.Id,
+                RecipeId = recipe2.Id,
+                Amount = 300,
+                MeasurementUnit = MeasurementUnit.Grams
+            };
+            appStorage.RecipeIngredients.Add(recipeIngredient1);
+            appStorage.RecipeIngredients.Add(recipeIngredient2);
+
+            // Act
+            HashSet<Recipe> result = bll.GetRecipesByIngredient(null, "Spaghetti");
+
+            // Assert
+            Assert.IsTrue(result.Contains(recipe1)); // Expecting recipe1 to be in the result
+            Assert.IsTrue(result.Contains(recipe2)); // Expecting recipe2 to be in the result
+            Assert.IsFalse(result.Contains(recipe3)); // Expecting recipe3 not to be in the result
         }
 
         [TestMethod]
--- UnitTestingA1Base/Data/BusinessLogicLayer.cs
+++ UnitTestingA1Base/Data/BusinessLogicLayer.cs
@@ -11,7 +11,7 @@ namespace UnitTestingA1Base.Data
             _appStorage = appStorage;
         }
 
-        public HashSet<Recipe> GetRecipesByIngredient(int? id, string? name)
+        public HashSet<Recipe> GetRecipesByIngredient(int? id, string name)
         {
             Ingredient ingredient;
             HashSet<Recipe> recipes = new HashSet<Recipe>();
@@ -20,50 +20,24 @@ namespace UnitTestingA1Base.Data
             {
                 ingredient = _appStorage.Ingredients.First(i => i.Id == id);
 
-                HashSet<RecipeIngredient> recipeIngredients = _appStorage.RecipeIngredients.Where(rI => rI.IngredientId == ingredient.Id).ToHashSet();
+                HashSet<RecipeIngredient> recipeIngredients = _appStorage.RecipeIngredients
+                    .Where(rI => rI.IngredientId == ingredient.Id)
+                    .ToHashSet();
 
-                recipes = _appStorage.Recipes.Where(r => recipeIngredients.Any(ri => ri.RecipeId == r.Id)).ToHashSet();
+                recipes = _appStorage.Recipes
+                    .Where(r => recipeIngredients.Any(ri => ri.RecipeId == r.Id))
+                    .ToHashSet();
             }
-
-            return recipes;
-        }
-
-        public HashSet<Recipe> GetRecipesByDietaryRestriction(int? id, string? name)
-        {
-            Ingredient ingredient;
-            HashSet<Recipe> recipes = new HashSet<Recipe>();
-
-            if (id != null)
+            else if (name != null)
             {
-                // Retrieve the ingredient by ID
-                ingredient = _appStorage.Ingredients.FirstOrDefault(i => i.Id == id);
+                ingredient = _appStorage.Ingredients.FirstOrDefault(i => i.Name.Contains(name));
 
                 if (ingredient != null)
                 {
-                    // Get all RecipeIngredient entries associated with the ingredient
                     HashSet<RecipeIngredient> recipeIngredients = _appStorage.RecipeIngredients
-                        .Where(ri => ri.IngredientId == ingredient.Id)
+                        .Where(rI => rI.IngredientId == ingredient.Id)
                         .ToHashSet();
 
-                    // Find recipes that have a relationship with the ingredient
-                    recipes = _appStorage.Recipes
-                        .Where(r => recipeIngredients.Any(ri => ri.RecipeId == r.Id))
-                        .ToHashSet();
-                }
-            }
-            else if (!string.IsNullOrEmpty(name))
-            {
-                // Retrieve the ingredient by name (case-insensitive search)
-                ingredient = _appStorage.Ingredients.FirstOrDefault(i => i.Name.Contains(name, StringComparison.OrdinalIgnoreCase));
-
-                if (ingredient != null)
-                {
-                    // Get all RecipeIngredient entries associated with the ingredient
-                    HashSet<RecipeIngredient> recipeIngredients = _appStorage.RecipeIngredients
-                        .Where(ri => ri.IngredientId == ingredient.Id)
-                        .ToHashSet();
-
-                    // Find recipes that have a relationship with the ingredient
                     recipes = _appStorage.Recipes
                         .Where(r => recipeIngredients.Any(ri => ri.RecipeId == r.Id))
                         .ToHashSet();
@@ -73,5 +47,6 @@ namespace UnitTestingA1Base.Data
             return recipes;
         }
 
+      
     }
 }
